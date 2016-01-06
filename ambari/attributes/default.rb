07d60d6e44b28019dbccfbae9708870dbb2a1b70
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-06 15:45:05 +0530

    Changed attribute file

--- ambari/attributes/default.rb
+++ ambari/attributes/default.rb
@@ -1,5 +1,3 @@
 default['ambari']['cent_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.0.0/ambari.repo'
-default['ambari']['admin_user'] = 'admin'
-default['ambari']['admin_password'] = 'admin'
-default['ambari']['version']  = '2.0.0'
+
 
