d2c4335e2832c88451e3d8663cde4c601d3ec03a
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-08 13:00:43 +0530

    Changed

--- ambari/attributes/default.rb
+++ ambari/attributes/default.rb
@@ -1,3 +1,3 @@
-default['ambari']['cent_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.0.0/ambari.repo'
-
+#default['ambari']['cent_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.0.0/ambari.repo'
+default['ambari']['cent_repo'] = 'http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.1.0/ambari.repo'
 
