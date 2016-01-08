c1bedecb44e26c480238780bb5eef5555c9a362a
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-08 13:28:29 +0530

    Changed

---
+++ ambari-server-ubuntu/README.md
@@ -0,0 +1,6 @@
+This cookbook is cooked and mantained by Minjar Cloud Solutions.
+Install and configure ambari 2.0.0
+
+Tested and supported on.
+
+CentOS release 6.5
---
+++ ambari-server-ubuntu/recipes/default.rb
@@ -0,0 +1,69 @@
+######AMBARI-SERVER #########
+execute 'update' do
+    command 'apt-get update'
+end
+
+#yum_repository = node['ambari']['repo']
+
+apt_repository 'Ambari' do
+    uri node['ambari-server-ubuntu']['repo']
+    distribution 'Ambari'
+    components ['main']
+    keyserver    'hkp://keyserver.ubuntu.com:80'
+    key          'B9733A7A07513CAD'
+  end
+
+apt_package 'ntp' do
+	action :install
+end
+
+execute 'chkconfig' do
+	command 'chkconfig ntp on'
+end
+
+service 'ntpd' do
+	action :start
+end
+
+#yum_package 'selinux-policy-targeted' do
+#	action :install
+#end
+
+#template '/etc/selinux/config' do
+#	source 'selinux.config.erb'
+#	owner 'root'
+#	group 'root'
+#end
+
+execute 'chkconfig' do
+	command 'chkconfig iptables off'
+end
+
+service 'iptables' do
+	action :stop
+end
+
+apt_package 'ambari-server' do
+	action :install
+end
+
+execute 'do setup' do
+	command 'ambari-server setup -s'
+end
+
+#service 'ambari-server' do
+	#supports :start => true, :stop => true, :restart => true, :status => true, :setup => true
+	#action [:enable, :start]
+#end
+
+execute 'start ambari-server' do
+	command '/usr/sbin/ambari-server start'
+end
+
+#include_recipe 'ambari::pub_add'
+
+template '/root/.ssh/id_rsa' do
+        source 'key.erb'
+        owner 'root'
+        group 'root'
+end
