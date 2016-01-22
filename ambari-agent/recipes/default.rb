985045b84116214425a30c6e8a213a6b383383d3
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-22 19:07:41 +0530

    Changed

--- ambari-agent/recipes/default.rb
+++ ambari-agent/recipes/default.rb
@@ -10,3 +10,10 @@ end
 execute 'remove restriction to login as root user from ambari server' do
     command "sed -i -r 's/.{155}//' /root/.ssh/authorized_keys"
 end
+
+file '/root/.ssh/temp.pub' do
+  content '#{node['ambari-agent']['ambari-server-key']}'
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
--- ambari-agent/recipes/pub_add.rb
+++ ambari-agent/recipes/pub_add.rb
@@ -1,6 +1,6 @@
 ruby_block "add_pub_key" do
   block do
-    to_append = File.read("/root/.ssh/hdp-cluster.pub")
+    to_append = File.read("/root/.ssh/temp.pub")
     File.open("/root/.ssh/authorized_keys", "a") do |handle|
     handle.puts to_append
 end
