ec684ccf14480a096108a8629ba2cd774f30e6c0
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-22 21:43:25 +0530

    Changed

--- ambari-server-mysql-new/recipes/pub_add.rb
+++
@@ -1,8 +0,0 @@
-ruby_block "add_pub_key" do
-  block do
-    to_append = File.read("/root/.ssh/hdp-cluster.pub")
-    File.open("/root/.ssh/authorized_keys", "a") do |handle|
-    handle.puts to_append
-end
-end
-end
