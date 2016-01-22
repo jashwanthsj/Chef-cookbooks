e8190ebb8a23fa20f7d11f9f080e82677fbb5f3f
jashwanthsj <jashwanth.sj@gmail.com>
2016-01-22 21:31:36 +0530

    Added grant.sql

--- ambari-server-mysql-new/recipes/default.rb
+++ ambari-server-mysql-new/recipes/default.rb
@@ -49,8 +49,7 @@ execute 'setup ambari-server' do
 end
 
 execute 'create schema for ambari' do
-        command "mysql -h 
-        #{node['ambari-server-mysql-new']['dbhostname']} -P 3306 -u #{node['ambari-server-mysql-new']['dbusername']} -p'#{node['ambari-server-mysql-new']['dbpasswd']}' #{node['ambari-server-mysql-new']['dbname']} <  /var/lib/ambari-server/resources/Ambari-DDL-MySQL-CREATE.sql"
+        command "mysql -h #{node['ambari-server-mysql-new']['dbhostname']} -P 3306 -u #{node['ambari-server-mysql-new']['dbusername']} -p'#{node['ambari-server-mysql-new']['dbpasswd']}' #{node['ambari-server-mysql-new']['dbname']} <  /var/lib/ambari-server/resources/Ambari-DDL-MySQL-CREATE.sql"
 end
 
 template '/tmp/grant.sql' do
@@ -61,8 +60,7 @@ template '/tmp/grant.sql' do
 end
 
 execute 'create schema for ambari' do
-        command "mysql -h 
-        #{node['ambari-server-mysql-new']['dbhostname']} -P 3306 -u #{node['ambari-server-mysql-new']['dbusername']} -p'#{node['ambari-server-mysql-new']['dbpasswd']}' #{node['ambari-server-mysql-new']['dbname']} <  /tmp/grant.sql"
+        command "mysql -h #{node['ambari-server-mysql-new']['dbhostname']} -P 3306 -u #{node['ambari-server-mysql-new']['dbusername']} -p'#{node['ambari-server-mysql-new']['dbpasswd']}' #{node['ambari-server-mysql-new']['dbname']} <  /tmp/grant.sql"
 end
 
 
